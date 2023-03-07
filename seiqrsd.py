aa77c08c5add42e40d0bb5bddd006e2a486b5f81
Saptak Das <66028584+Saptak625@users.noreply.github.com>
2023-03-06 20:05:35 -0500

    Newest Policy Graphs using SEIQRSD

--- seiqrsd.py
+++ seiqrsd.py
@@ -6,20 +6,20 @@ from r_plot import r_plot
 from model import deriv
 
 # Test Case
-save = False
-test_case = 'High Population'
+save = True
+test_case = 'Quarantine'
 print(f'Test Case: {test_case}')
 
 # Total population, N.
-N = 100000
+N = 1000
 # Initial number of infected and recovered individuals, I0 and R0.
 E0, I0, Q0, R0, D0 = 0, 1, 0, 0, 0
 # Everyone else, S0, is susceptible to infection initially.
-S0 = N - E0 - I0 - Q0 - R0 
+S0 = N - E0 - I0 - Q0 - R0 - D0
 # Contact rate(beta), incubation period(phi, in 1/days), quarantine rate(zeta, in 1/days), mean recovery rate in non-quarantine(gamma, in 1/days), mean recovery rate in quarantine(kappa, in 1/days), and immunity wearoff rate(mu, in 1/days).
-beta, phi, zeta, gamma, kappa, mu, alpha, epsilon = 0.9, 1./10, (1./10), 1./10, 1./10, (1./60), 0.1, 0.1
+beta, phi, zeta, gamma, kappa, mu, alpha, epsilon = 0.9, 1./10, 1.3 * (1./10), 1./10, 1./10, (1./60), 0.002, 0.002
 # A grid of time points (in days)
-t = np.linspace(0, 1600, 1600)
+t = np.linspace(0, 150, 150)
 
 print(f'Beta: {beta}, Gamma: {gamma}')
 r0 = r_plot(S0, N, beta, gamma, kappa, alpha)
@@ -38,8 +38,9 @@ Ie = (phi*kappa*mu*N*(beta-gamma-zeta))/(beta*((kappa*(gamma+zeta)*(mu+phi))+(ph
 Ee = (beta*Se*Ie)/(N*phi)
 Qe = (zeta*Ie) / kappa
 Re = (Ie*(gamma+zeta))/mu
+De = 0
 
-print(f'Se: {Se}, Ee: {Ee}, Ie: {Ie}, Qe: {Qe}, Re: {Re}')
+print(f'Se: {Se}, Ee: {Ee}, Ie: {Ie}, Qe: {Qe}, Re: {Re}, De: {D[-1]}')
 
 print(f'Imax = {max(I)}')
 print(f'Cases(max) = {max(I+Q)}')
@@ -69,9 +70,9 @@ plt.show(block=False)
 
 #Plot Stacked Area Graph
 plt.figure()
-plt.stackplot(t, E, I, Q, S, R, D, labels=['Exposed', 'Infected', 'Quarantined','Susceptible','Recovered'])
+plt.stackplot(t, E, I, Q, S, R, D, labels=['Exposed', 'Infected', 'Quarantined','Susceptible','Recovered', 'Dead'])
 plt.legend(loc='upper right')
-plt.title(f'Stacked SEIQRS States over Time ({test_case} Case)')
+plt.title(f'Stacked SEIQRSD States over Time ({test_case} Case)')
 plt.xlabel('Time (Days)')
 plt.ylabel('Amount of Population (People)')
 plt.autoscale()
@@ -87,13 +88,14 @@ plt.plot(t, I, label = "Infected")
 plt.plot(t, Q, label = "Quarantined")
 plt.plot(t, R, label = "Recovered")
 plt.plot(t, D, label = "Dead")
-# plt.axhline(y = Se, linestyle = '--')
-# plt.axhline(y = Ee, linestyle = '--')
-# plt.axhline(y = Ie, linestyle = '--')
-# plt.axhline(y = Qe, linestyle = '--')
-# plt.axhline(y = Re, linestyle = '--')
+plt.axhline(y = Se, linestyle = '--')
+plt.axhline(y = Ee, linestyle = '--')
+plt.axhline(y = Ie, linestyle = '--')
+plt.axhline(y = Qe, linestyle = '--')
+plt.axhline(y = Re, linestyle = '--')
+plt.axhline(y = De, linestyle = '--')
 plt.legend(loc='upper right')
-plt.title(f'SEIQRS State over Time ({test_case} Case)')
+plt.title(f'SEIQRSD State over Time ({test_case} Case)')
 plt.xlabel('Time (Days)')
 plt.ylabel('Amount of Population (People)')
 plt.autoscale()
